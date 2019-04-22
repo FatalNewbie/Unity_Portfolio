43759c7c3489b3b71cb08c7954c482bc44645ea1
FatalNewbie <48378620+FatalNewbie@users.noreply.github.com>
2019-04-22 20:05:09 +0900

    Boss_Adjust_7
    0422
    
    Velocity 이거 뭔가 엄청나다.
    보스의 점프 공격을 위해 포물선운동을 매 프레임 위치를 찍어서 이동하는게 아니라 힘을 이용해서 나타내고 싶었다. 점프 시작할때 1번만 힘을 가해 나머지는 자유운동.
    근데 Velocity이게 Vector3(x,y,z) 중에 저 y값 고정해 놓고 x,z값에 어떤 값을 주더라도 항상 같은 높이까지만 올라가네.
    더 신기한건 x=1,z=0 을 줬을 때 떨어진 위치가 x=10이라 가정, x=0, z=1 을 주면 마찬가지로 z=10에 떨어진다.
    그리고 x=1,z=1 을 주면 (10,10)에 딱 떨어진다.
    물체를 내가 원하는 위치의 근사값에 놓이게 하는게 생각보다는 어렵지 않을듯 하다.
    
    힘을 이용한 포물선 점프 준비는 완료이고, 이제 BossController 수정 해야함.

--- MiddleAgesWar/Assets/Scripts/Parabolic_jump_test.cs
+++ MiddleAgesWar/Assets/Scripts/Parabolic_jump_test.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Parabolic_jump_test : MonoBehaviour {
 
     bool mSwitch;
     Rigidbody mRigid;
+
+   
 
 	// Use this for initialization
 	void Start () {
@@ -17,16 +17,17 @@ public class Parabolic_jump_test : MonoBehaviour {
 	void Update () {
         if (mSwitch)
             Fire();
-        if (!mSwitch)
-        {
-            if (mRigid.velocity == Vector3.zero)
-                Debug.Log("Done!!");
-        }
+        
+
+        
 	}
 
     void Fire()
     {
-        mRigid.velocity = new Vector3(20, 10, 0);
+        Vector3 mVec = new Vector3(-1.02f, 24, 0.82f);
+
+        mRigid.velocity = mVec;
+
         //mRigid.AddForce(new Vector3(0, 10, 0));
         mSwitch = false;
     }
